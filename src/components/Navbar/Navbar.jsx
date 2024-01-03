bda9658b6e351adcd633fa6c68c6e330c8295146
Noah Rodgers <noah@createdbynoah.com>
2024-01-03 11:19:50 -0600

    add Works section for portfolio projects

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
         <img src={images.logo} alt="logo" />
       </div>
       <ul className="app__navbar-links">
-        {['home', 'about', 'works', 'testimonials', 'contact'].map((item) => (
+        {['home', 'about', 'work', 'testimonials', 'contact'].map((item) => (
           <li className="app__flex p-text" key={`link-${item}`}>
             <div></div>
             <a href={`#${item}`}>{item}</a>
