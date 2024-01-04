e1b161e73f372dd729f290f23484d9b307c8a252
Noah Rodgers <noah@createdbynoah.com>
2024-01-04 08:12:08 -0600

    remove copyright from AppWrap

--- src/wrapper/AppWrap.jsx
+++ src/wrapper/AppWrap.jsx
@@ -9,10 +9,6 @@ const AppWrap = (Component, idName, classNames) =>
         <SocialMedia />
         <div className="app__wrapper app__flex">
           <Component />
-          <div className="copyright">
-            <p className="p-text">&copy; 2024 Noah Rodgers</p>
-            <p className="p-text">All rights reserved</p>
-          </div>
         </div>
         <NavigationDots active={idName} />
       </div>
