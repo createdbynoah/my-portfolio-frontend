dde0fe8665ee684b79c61fc73b9b7f62283141ab
Noah Rodgers <noah@createdbynoah.com>
2024-01-04 13:29:47 -0600

    Refactor header component and update styles

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { motion } from 'framer-motion';
+import { GoArrowDownRight } from 'react-icons/go';
 
-import { AppWrap } from '../../wrapper';
+import { AppWrap, MotionWrap } from '../../wrapper';
 import { images } from '../../constants';
 import './Header.scss';
 
@@ -18,57 +19,77 @@ const scaleVariants = {
 
 const Header = () => {
   return (
-    <div className="app__header app__flex">
-      <motion.div
-        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
-        transition={{ duration: 0.8 }}
-        className="app__header-info"
-      >
-        <div className="app__header-badge">
-          <div className="badge-cmp app__flex">
-            <span>👋</span>
-            <div style={{ marginLeft: 20 }}>
-              <p className="p-text">Hello, I am</p>
-              <h1 className="head-text">Noah</h1>
-            </div>
-          </div>
-          <div className="tag-cmp app__flex">
-            <p className="p-text">Full-Stack Developer</p>
-            <p className="p-text">Designer</p>
-          </div>
-        </div>
-      </motion.div>
-      <motion.div
-        whileInView={{ opacity: [0, 1] }}
-        transition={{ duration: 0.5, delayChildren: 0.5 }}
-        className="app__header-img"
-      >
+    <div className="grid__main">
+      <div className="item-a">
+        <h1>
+          Hi, I'm <span>Noah</span>.
+        </h1>
+        <h2>Full-Stack Developer</h2>
+      </div>
+      <div className="item-b">
         <img src={images.noah} alt="profile_bg" />
-        <motion.img
-          whileInView={{ scale: [0, 1] }}
-          transition={{ duration: 1, ease: 'easeInOut' }}
-          src={images.circle}
-          alt="profile_circle"
-          className="overlay_circle"
+      </div>
+      <div className="item-c">
+        <img
+          src="https://i.insider.com/61dc71461025b20018bb0597?width=700"
+          alt=""
         />
-      </motion.div>
-      <motion.div
-        variant={scaleVariants}
-        whileInView={scaleVariants.whileInView}
-        className="app__header-circles"
-      >
-        {[images.flutter, images.redux, images.sass].map((item, index) => (
-          <div
-            key={`circle-${index}`}
-            alt="circle"
-            className="circle-cmp app__flex"
-          >
-            <img src={item} alt="circle" />
-          </div>
-        ))}
-      </motion.div>
+      </div>
+      <div className="item-d">
+        <GoArrowDownRight />
+      </div>
     </div>
+    // <div className="app__header app__flex">
+    //   <motion.div
+    //     whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+    //     transition={{ duration: 0.8 }}
+    //     className="app__header-info"
+    //   >
+    //     <div className="app__header-badge">
+    //       <div className="badge-cmp app__flex">
+    //         <span>👋</span>
+    //         <div style={{ marginLeft: 20 }}>
+    //           <p className="p-text">Hello, I am</p>
+    //           <h1 className="head-text">Noah</h1>
+    //         </div>
+    //       </div>
+    //       <div className="tag-cmp app__flex">
+    //         <p className="p-text">Full-Stack Developer</p>
+    //         <p className="p-text">Designer</p>
+    //       </div>
+    //     </div>
+    //   </motion.div>
+    //   <motion.div
+    //     whileInView={{ opacity: [0, 1] }}
+    //     transition={{ duration: 0.5, delayChildren: 0.5 }}
+    //     className="app__header-img"
+    //   >
+    //     <img src={images.noah} alt="profile_bg" />
+    //     <motion.img
+    //       whileInView={{ scale: [0, 1] }}
+    //       transition={{ duration: 1, ease: 'easeInOut' }}
+    //       src={images.circle}
+    //       alt="profile_circle"
+    //       className="overlay_circle"
+    //     />
+    //   </motion.div>
+    //   <motion.div
+    //     variant={scaleVariants}
+    //     whileInView={scaleVariants.whileInView}
+    //     className="app__header-circles"
+    //   >
+    //     {[images.flutter, images.redux, images.sass].map((item, index) => (
+    //       <div
+    //         key={`circle-${index}`}
+    //         alt="circle"
+    //         className="circle-cmp app__flex"
+    //       >
+    //         <img src={item} alt="circle" />
+    //       </div>
+    //     ))}
+    //   </motion.div>
+    // </div>
   );
 };
 
-export default AppWrap(Header, 'home');
+export default AppWrap(MotionWrap(Header, 'app__header'), 'home');
