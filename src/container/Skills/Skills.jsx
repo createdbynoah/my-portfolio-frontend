eb3e44869b73023ee64f24f570f65a0d457a90d3
Noah Rodgers <noah@createdbynoah.com>
2024-01-22 23:04:24 -0600

    Update Skills component with new workExperiences prop

--- src/container/Skills/Skills.jsx
+++ src/container/Skills/Skills.jsx
@@ -9,7 +9,7 @@ import './Skills.scss';
 import { MoreLink } from '../../components';
 
 const Skills = () => {
-  const { skills, experiences } = useSanityContext();
+  const { skills, workExperiences } = useSanityContext();
 
   return (
     <>
@@ -24,10 +24,7 @@ const Skills = () => {
               className="app__skills-item app__flex"
               key={skill.name}
             >
-              <div
-                className="app__flex"
-                style={{ backgroundColor: skill.bgColor }}
-              >
+              <div className="app__flex">
                 <img src={urlFor(skill.icon)} alt={skill.name} />
               </div>
               <p className="p-text">{skill.name}</p>
@@ -35,29 +32,29 @@ const Skills = () => {
           ))}
         </motion.div>
         <motion.div className="app__skills-exp">
-          {experiences?.map((exp) => (
-            <motion.div className="app__skills-exp-item" key={exp.year}>
+          {workExperiences?.map((exp, index) => (
+            <motion.div className="app__skills-exp-item" key={exp._id}>
               <div className="app__skills-exp-year">
-                <p className="bold-text">{exp.year}</p>
+                <p className="bold-text">
+                  {index <= 1 &&
+                    `${exp.startDateDisp.full} - ${exp.endDateDisp.full}`}
+                  {index > 1 &&
+                    `${exp.startDateDisp.year} - ${exp.endDateDisp.year}`}
+                </p>
               </div>
               <motion.div className="app__skills-exp-works">
-                {exp.works.map((work) => (
-                  <motion.div
-                    whileInView={{ opacity: [0, 1] }}
-                    transition={{ duration: 0.5 }}
-                    className="app__skills-exp-work"
-                    data-tooltip-id={work.name}
-                    data-tooltip-content={work.description}
-                    key={work.name}
-                  >
-                    <h4 className="bold-text">{work.name}</h4>
-                    <p className="p-text">{work.company}</p>
-                    <Tooltip
-                      id={work.name}
-                      className="skills-tooltip"
-                    ></Tooltip>
-                  </motion.div>
-                ))}
+                <motion.div
+                  whileInView={{ opacity: [0, 1] }}
+                  transition={{ duration: 0.5 }}
+                  className="app__skills-exp-work"
+                  // data-tooltip-id={exp.name}
+                  // data-tooltip-content={exp.responsibilities}
+                  key={exp.name}
+                >
+                  <h4 className="bold-text">{exp.jobTitle}</h4>
+                  <p className="p-text">{exp.company}</p>
+                  {/* <Tooltip id={exp.name} className="skills-tooltip"></Tooltip> */}
+                </motion.div>
               </motion.div>
             </motion.div>
           ))}
