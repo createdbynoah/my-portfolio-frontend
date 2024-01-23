5e2c904f803a1e8633d66a8434d3876ea20920f1
Noah Rodgers <noah@createdbynoah.com>
2024-01-22 23:55:11 -0600

    Add ImageHeading component and ProjectPage styling

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -1,33 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { PortableText } from '@portabletext/react';
 
 import { urlFor } from '../utils/client';
 import { useSanityContext } from '../context/SanityContext';
+import { ImageHeading } from '../components';
+
+import './ProjectPage.scss';
 
 const ProjectPage = () => {
   const { projects } = useSanityContext();
   const { slug } = useParams();
 
   const project = projects.find((project) => project.slug.current === slug);
-
   return (
-    <div className="app__container col">
-      <header className="projects__heading">
-        <h1>{project.name}</h1>
-      </header>
+    <div className="app__container col ">
+      <ImageHeading
+        image={urlFor(project?.heroImageUrl)}
+        title={project?.name}
+        height={350}
+        bgPosition={[50, 50]}
+      />
       <section className="project__container">
-        <div className="project__image">
-          <img src={urlFor(project.heroImageUrl)} alt={project.name} />
-        </div>
+        <div className="project__details"></div>
         <div className="project__content">
           <div className="project__description">
-            <PortableText value={project.description_long} />
-          </div>
-          <div className="project__links">
-            <a href={project.link} target="_blank" rel="noreferrer">
-              <button className="app__button">View Project</button>
-            </a>
+            <PortableText value={project?.description_long} />
           </div>
         </div>
       </section>
