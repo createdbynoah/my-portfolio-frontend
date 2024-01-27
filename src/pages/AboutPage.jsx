3756e52834b3ab97170c6fc058ebdf57eea1e306
Noah Rodgers <noah@createdbynoah.com>
2024-01-26 20:24:25 -0600

    Refactor AboutPage component and styles, add content

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -21,37 +21,35 @@ const AboutPage = () => {
           <div className="about__noah">
             <div className="about__noah-text">
               <h2>Noah is...</h2>
-              <p>
-                <ul>
-                  <li>
-                    engaging and communicative. I love to learn by listening and
-                    asking good questions.
-                  </li>
-                  <li>a critical thinker and a natural problem solver.</li>
-                  <li>
-                    a collaborator — always looking for ways to help others
-                    around him.
-                  </li>
-                  <li>
-                    expressive and energetic — definitely the guy at the party
-                    in the very middle causing a scene in an effort to ensure
-                    people are having a good time.
-                  </li>
-                  <li>
-                    living right on the edge of spontaneous and slightly
-                    terrifying. I'd rather be in the middle of the action than
-                    watching from the sidelines.
-                  </li>
-                  <li>
-                    a glass-half-full kinda guy. I tend to be optimistic and
-                    hopeful (with a healthy dose of realism when it counts).
-                  </li>
-                  <li>
-                    motivated by like-minded individuals with a high sense of
-                    excellence and a natural bent towards curiousity.
-                  </li>
-                </ul>
-              </p>
+              <ul>
+                <li>
+                  engaging and communicative. I love to learn by listening and
+                  asking good questions.
+                </li>
+                <li>a critical thinker and a natural problem solver.</li>
+                <li>
+                  a collaborator — always looking for ways to help others around
+                  him.
+                </li>
+                <li>
+                  expressive and energetic — definitely the guy at the party in
+                  the very middle causing a scene in an effort to ensure people
+                  are having a good time.
+                </li>
+                <li>
+                  living right on the edge of spontaneous and slightly
+                  terrifying. I'd rather be in the middle of the action than
+                  watching from the sidelines.
+                </li>
+                <li>
+                  a glass-half-full kinda guy. I tend to be optimistic and
+                  hopeful (with a healthy dose of realism when it counts).
+                </li>
+                <li>
+                  motivated by like-minded individuals with a high sense of
+                  excellence and a natural bent towards curiousity.
+                </li>
+              </ul>
             </div>
             <div className="about__noah-images">
               <img src={images.colorado1} />
@@ -71,9 +69,7 @@ const AboutPage = () => {
 
               <p>
                 I perform well under pressure and I'm a quick learner. I thrive
-                in the spotlight and can hold my own in front of a crowd. I'm .
-                I love to collaborate with those around me and I'm always
-                looking for ways to help others.
+                in the spotlight and can hold my own in front of a crowd.
               </p>
             </div>
             <div className="about__interests-images">
