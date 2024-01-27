4d37819644cb12415397f1905c53bf0c29256be4
Noah Rodgers <noah@createdbynoah.com>
2024-01-26 20:21:01 -0600

    Add contact constants file

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,7 @@
 import images from './images';
+import contact from './contact';
 
 const emailUrl = `${import.meta.env.VITE_EMAIL_SERVER_URL}/api/send-email`;
 
-export { emailUrl, images };
+export { emailUrl, images, contact };
 
