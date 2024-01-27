b446bc050c0289a60295bd796a811f356496e7a3
Noah Rodgers <noah@createdbynoah.com>
2024-01-27 14:44:23 -0600

    Add ListItem component, add Work section content, and update styles in AboutPage.jsx for mobile viewing

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import useMediaQuery from '@mui/material/useMediaQuery';
 
-import { ImageHeading } from '../components';
+import { ImageHeading, ListItem } from '../components';
 import { images } from '../constants';
 
 import './AboutPage.scss';
@@ -22,33 +22,35 @@ const AboutPage = () => {
             <div className="about__noah-text">
               <h2>Noah is...</h2>
               <ul>
-                <li>
+                <ListItem>
                   engaging and communicative. I love to learn by listening and
                   asking good questions.
-                </li>
-                <li>a critical thinker and a natural problem solver.</li>
-                <li>
+                </ListItem>
+                <ListItem>
+                  a critical thinker and a natural problem solver.
+                </ListItem>
+                <ListItem>
                   a collaborator — always looking for ways to help others around
                   him.
-                </li>
-                <li>
+                </ListItem>
+                <ListItem>
+                  motivated by like-minded individuals with a high sense of
+                  excellence and a natural bent towards curiousity.
+                </ListItem>
+                <ListItem>
                   expressive and energetic — definitely the guy at the party in
                   the very middle causing a scene in an effort to ensure people
-                  are having a good time.
-                </li>
-                <li>
+                  have a good time.
+                </ListItem>
+                <ListItem>
                   living right on the edge of spontaneous and slightly
                   terrifying. I'd rather be in the middle of the action than
                   watching from the sidelines.
-                </li>
-                <li>
+                </ListItem>
+                <ListItem>
                   a glass-half-full kinda guy. I tend to be optimistic and
                   hopeful (with a healthy dose of realism when it counts).
-                </li>
-                <li>
-                  motivated by like-minded individuals with a high sense of
-                  excellence and a natural bent towards curiousity.
-                </li>
+                </ListItem>
               </ul>
             </div>
             <div className="about__noah-images">
@@ -66,14 +68,27 @@ const AboutPage = () => {
                 into the chaos. I'm a classically-trained pianist and love to
                 play the piano to relax or help me think through a problem.
               </p>
-
-              <p>
-                I perform well under pressure and I'm a quick learner. I thrive
-                in the spotlight and can hold my own in front of a crowd.
-              </p>
             </div>
             <div className="about__interests-images">
               <img src={images.colorado2} />
+            </div>
+          </div>
+          <div className="about__work">
+            <div className="about__work-text">
+              <h2>At Work</h2>
+              <p>
+                I'm a quick learner and perform well under pressure. I thrive in
+                the spotlight and can hold my own in front of a crowd. I'm able
+                to effectively communicate with stakeholders, bridging the
+                translation layer between the technical and non-technical. I
+                love working towards an achievable goal and grasping the vision
+                behind my contributions. I thrive under leadership who sees my
+                potential and pushes me to grow into it, offering guidance and
+                support along the way.
+              </p>
+            </div>
+            <div className="about__work-images">
+              <img src={images.noahStudio} />
             </div>
           </div>
         </div>
