51075b8037252a4e77ca31165f2fd5d0b271a531
Noah Rodgers <noah@createdbynoah.com>
2024-01-27 15:38:08 -0600

    Remove unused images

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,44 +1,10 @@
-import email from '../assets/images/email.png';
-import mobile from '../assets/images/mobile.png';
-import api from '../assets/images/api.png';
-import cpp from '../assets/images/cpp.png';
-import css from '../assets/images/css.png';
-import figma from '../assets/images/figma.png';
-import flutter from '../assets/images/flutter.png';
-import git from '../assets/images/git.png';
-import graphql from '../assets/images/graphql.png';
-import html from '../assets/images/html.png';
-import javascript from '../assets/images/javascript.png';
-import mu5 from '../assets/images/mu5.png';
-import node from '../assets/images/node.png';
-import python from '../assets/images/python.png';
-import react from '../assets/images/react.png';
-import redux from '../assets/images/redux.png';
-import sass from '../assets/images/sass.png';
-import typescript from '../assets/images/typescript.png';
-import vue from '../assets/images/vue.png';
-
-import about01 from '../assets/images/about01.png';
-import about02 from '../assets/images/about02.png';
-import about03 from '../assets/images/about03.png';
-import about04 from '../assets/images/about04.png';
-
 import noah from '../assets/images/noah.png';
 import noahSquare from '../assets/images/noah-square.png';
-import profile from '../assets/images/profile.png';
 import circle from '../assets/images/circle.svg';
 import logoLight from '../assets/images/logos/noahrodgers-full-light.svg';
 import logoDark from '../assets/images/logos/noahrodgers-full-dark.svg';
 import markLight from '../assets/images/logos/noahrodgers-initials-light.svg';
 import markDark from '../assets/images/logos/noahrodgers-initials-dark.svg';
-
-import adidas from '../assets/images/adidas.png';
-import amazon from '../assets/images/amazon.png';
-import asus from '../assets/images/asus.png';
-import bolt from '../assets/images/bolt.png';
-import nb from '../assets/images/nb.png';
-import skype from '../assets/images/skype.png';
-import spotify from '../assets/images/spotify.png';
 
 import blocks from '../assets/images/blocks.png';
 import pegboard from '../assets/images/pegboard.png';
@@ -49,44 +15,13 @@ import colorado2 from '../assets/images/colorado2.webp';
 import noahStudio from '../assets/images/noah-studio.jpg';
 
 export default {
-  email,
-  mobile,
-  api,
-  cpp,
-  css,
-  figma,
-  flutter,
-  git,
-  graphql,
-  html,
-  javascript,
-  mu5,
-  node,
   noah,
   noahSquare,
-  python,
-  react,
-  redux,
-  sass,
-  typescript,
-  vue,
-  about01,
-  about02,
-  about03,
-  about04,
-  profile,
   circle,
   logoLight,
   logoDark,
   markLight,
   markDark,
-  adidas,
-  amazon,
-  asus,
-  bolt,
-  nb,
-  skype,
-  spotify,
   blocks,
   pegboard,
   family,
@@ -94,4 +29,4 @@ export default {
   colorado1,
   colorado2,
   noahStudio,
-};
+}
