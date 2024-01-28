7ec6bcd9cc93248d4effa0067f85684e88ea58ff
Noah Rodgers <noah@createdbynoah.com>
2024-01-27 21:30:44 -0600

    Add NotFoundPage content and styling

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './NotFoundPage.scss';
 
 const NotFoundPage = () => {
   return (
-    <div>
-      <h1>NotFoundPage</h1>
-      <h1>NotFoundPage</h1>
-      <h1>NotFoundPage</h1>
-      <h1>NotFoundPage</h1>
-      <h1>NotFoundPage</h1>
-      <h1>NotFoundPage</h1>
+    <div className="app__container col app__primarybg">
+      <div className="app__not-found">
+        <h1>404</h1>
+        <h2>Sorry... there's nothing here.</h2>
+        <p>
+          Return to <Link to="/">Home</Link>
+        </p>
+      </div>
     </div>
   );
 };
